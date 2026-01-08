8e03e1fff68bfa55c803924c6c05fd56838b7d18
冬桜子 濱元 <toko0124615@icloud.com>
2026-01-08 11:21:23 +0900

    a

--- Program.cs
+++ Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Sample
+{
+    static void Main()
+    {
+        DateTime Today = DateTime.Now;
+        Console.WriteLine(Today.ToString("yyyy/MM/dd HH:mm:ss"));
+    }
+
+
+}
